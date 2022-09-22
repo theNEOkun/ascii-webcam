a9dde8c9b63976c181c5849933d601b17a178e81
theNEOkun <sven2000@student.miun.se>
2022-09-22 23:07:40 +0200

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod terminal;
 
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
+use image::{DynamicImage, GenericImageView};
 use nokhwa::{Camera, CameraFormat, CaptureAPIBackend, FrameFormat};
-use std::{io, ops::Deref};
 use terminal::Term;
 
 const ASCII: [&str; 32] = [
@@ -10,48 +9,38 @@ const ASCII: [&str; 32] = [
     ";", ":", "+", "=", "-", ",", ".", "_", " ", " ", " ", " ", " ",
 ];
 
-const ASCII_REV: [&str; 28] = [
-    " ", "_", ".", ",", "-", "=", "+", ":", ";", "c", "b", "a", "?", "0", "1", "2", "3", "4", "5",
-    "6", "7", "8", "9", "$", "W", "#", "@", "Ñ",
-];
-
 const ASCII_LEN: isize = ASCII.len() as isize - 1;
 
+/// Gets the specified ascii-character
 fn get_ascii(intensity: isize) -> &'static str {
     ASCII[(intensity) as usize]
 }
 
-fn map(number: isize, start1: isize, end1: isize, start2: isize, end2: isize) -> isize {
-    start2 + (number - start1) * (end2 - start2) / (end1 - start1)
+/// Maps a number from an old range to a new range
+///
+/// * number - is the number to map
+/// * from - is the start-range
+/// * to - is the stop-range
+///
+/// * returns - an isize
+fn map(number: isize, from: (isize, isize), to: (isize, isize)) -> isize {
+    to.0 + (number - from.0) * (to.1 - to.0) / (from.1 - from.0)
 }
 
-fn handle_image(term: &mut Term, image: DynamicImage, scale: u32) {
+/// Function which handles the image to ascii-conversion
+///
+/// * term - is the terminal to use
+/// * image - is the image to ascii-fy
+fn handle_image(term: &mut Term, image: DynamicImage) {
     let (width, height) = image.dimensions();
 
     term.draw(&mut |term| {
         for y_pos in 0..height {
             for x_pos in 0..width {
-                // if y_pos % (scale * 2) == 0 && x_pos % scale == 0 {
-                    let pixel = image.get_pixel(x_pos, y_pos);
-                    let intensity = pixel[0] / 3 + pixel[1] / 3 + pixel[2] / 3;
-                    let char_index = map(intensity.into(), 0, 255, ASCII_LEN, 0);
-                    term.put_pixel(x_pos, y_pos, get_ascii(char_index));
-                // }
-            }
-        }
-    });
-}
-
-fn handle_image_bytes(term: &mut Term, image: &[u8], scale: usize, width: u32, height: u32) {
-    term.draw(&mut |term| {
-        for y_pos in (0..height).step_by(scale) {
-            for x_pos in (0..width).step_by(scale) {
-                let pixel_index = ((y_pos + x_pos * width) / 2) as usize;
-                let avg = image[pixel_index + 0] as isize;
-                let char_index = map(avg, 0, 255, 0, ASCII_LEN);
-                //println!("{char_index}: {avg}");
+                let pixel = image.get_pixel(x_pos, y_pos);
+                let intensity = pixel[0] / 3 + pixel[1] / 3 + pixel[2] / 3;
+                let char_index = map(intensity.into(), (0, 255), (ASCII_LEN, 0));
                 term.put_pixel(x_pos, y_pos, get_ascii(char_index));
-                //term.put_pixel(x_pos, y_pos, &char_index.to_string());
             }
         }
     });
@@ -61,19 +50,18 @@ fn main() {
     let mut term = Term::new();
     let mut camera = Camera::with_backend(
         0,
-        Some(CameraFormat::new_from(
-            640,
-            320,
-            FrameFormat::YUYV,
-            30,
-        )),
+        Some(CameraFormat::new_from(640, 320, FrameFormat::YUYV, 30)),
         CaptureAPIBackend::Video4Linux,
     )
     .unwrap();
     camera.open_stream().unwrap();
     loop {
-        let frame = DynamicImage::ImageRgb8(camera.frame().expect("Stream is dead")).resize(80, 60, image::imageops::FilterType::Nearest);//.resize(term.width.into(), term.height.into(), image::imageops::FilterType::Nearest);
+        let frame = DynamicImage::ImageRgb8(camera.frame().expect("Stream is dead")).resize(
+            80,
+            60,
+            image::imageops::FilterType::Nearest,
+        );
 
-        handle_image(&mut term, frame, 4);
+        handle_image(&mut term, frame);
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -6,6 +6,10 @@ use crossterm::{
 };
 use std::io::{stdout, Stdout, Write};
 
+/// Struct that handles the terminal
+/// * stdout - is the standard output
+/// * width - is the width of the terminal
+/// * height - is the height of the terminal
 pub struct Term {
     stdout: Stdout,
     pub width: u16,
@@ -13,6 +17,7 @@ pub struct Term {
 }
 
 impl Term {
+    /// Creates a new terminal, with everything set
     pub fn new() -> Self {
         let stdout = stdout();
         let (column, row) = terminal::size().unwrap();
@@ -23,29 +28,36 @@ impl Term {
         }
     }
 
+    /// Private function to clear the screen
     fn clear(&mut self) {
         self.stdout
             .execute(terminal::Clear(terminal::ClearType::All))
             .expect("Could not clear the screen");
     }
 
+    /// Flushes the screen
     fn flush(&mut self) {
         self.stdout.flush().expect("Could not flush the screen");
     }
 
+    /// Function to draw to the sreen
+    ///
+    /// * function - is a function which takes the terminal and draws to it, clears and flushes
+    /// before and after
     pub fn draw(&mut self, function: &mut dyn FnMut(&mut Self)) -> () {
         self.clear();
         function(self);
         self.flush();
     }
 
+    /// Function which puts a string at an x and y
     pub fn put_pixel(&mut self, x: u32, y: u32, what: &str) {
         self.stdout
             .queue(cursor::MoveTo(x as u16, y as u16))
             .expect("Something went wrong when drawing the circle")
             .queue(cursor::Hide)
             .expect("Could not hide the cursor")
-            .queue(PrintStyledContent(what.red()))
+            .queue(PrintStyledContent(what.white()))
             .expect("Something went wrong with the coloring");
     }
 }
